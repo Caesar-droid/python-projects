5f51e3ca7694bf8e5dfebd9b63080f38d2f06f15
domie <dominictoroitichh@gmail.com>
2021-01-21 16:01:32 +0300

    cd changes

--- pets.py
+++ pets.py
@@ -8,4 +8,4 @@ def describe_pet(animal_type,pet_name):
 
     print(f"\nI have a {animal_type}")
     print(f"My {animal_type}'s name is {pet_name}")
-describe_pet('hamster','harry')
+describe_pet('hamster','poop')
