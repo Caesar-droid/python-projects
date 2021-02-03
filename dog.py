0e2110d9dd0232979779e751b956967b261fc056
domie <dominictoroitichh@gmail.com>
2021-02-03 11:30:51 +0300

    working with classes

---
+++ dog.py
@@ -0,0 +1,16 @@
+#we are going to create a class Dog that stores information about name,age and other functions common to dogs
+class Dog:
+    """A simple attempt to model a dog."""
+    def __init__(self,name,age):
+        """Initialize name and age attributes."""
+        self.name=name
+        self.age=age
+    def sit(self):
+        """Simulate a dog sitting in response to a command."""
+        print(f"{self.name} is now sitting")
+    def roll_over(self):
+        """Simulate rolling over in response to a command."""
+        print(f"{self.name} rolled over")
+my_dog=Dog('Willie',6)
+print(f"My dog's name is {my_dog.name}")
+print(f"My dog is {my_dog.age} years old")
