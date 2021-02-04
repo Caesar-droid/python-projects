de8ae45779c592a137aa8be0b34b79097a8dfa41
domie <dominictoroitichh@gmail.com>
2021-02-04 18:05:06 +0300

    using functions in class

--- Restaurant.py
+++ Restaurant.py
@@ -4,16 +4,23 @@ class Restaurant:
         """we are going to initialize attributes"""
         self.restaurant_name=restaurant_name
         self.cuisine_type=cuisine_type
+        self.number_served=0
     def describe_restaurant(self):
         """we are creating a function that describes""" 
         print(f"\nWe are {self.restaurant_name} restaurant.")
         print(f"We are offering {self.cuisine_type} cuisine.")
+        print(f"The restaurant has served {self.number_served} customers")
     def open_restaurant(self):
         """we are creating a function that displays we are open"""
         print(f"{self.restaurant_name.title()}, we are now open")
+    def set_number_served(self,number):
+        self.number_served=number
 restaurant=Restaurant('beans & cream','korean')
 restaurant.describe_restaurant()
 restaurant.open_restaurant()
+restaurant.set_number_served(40)
+restaurant.describe_restaurant()
+
 
 restaurant_1=Restaurant("CJ's",'chinesse')
 restaurant_1.describe_restaurant()
--- car.py
+++ car.py
@@ -4,6 +4,7 @@ for car in cars:
         print(car.upper())
     else :
         print(car.title())
+#implementing functions in class
 
 class Car:
     """A simple attempt to represent a car."""
@@ -12,10 +13,36 @@ class Car:
         self.make=make
         self.model=model
         self.year=year
+        self.odometer_reading=100
     def get_descriptive_name(self):
         """Return a neatly formatted descriptive name."""
         long_name=f"{self.year} {self.make} {self.model}"
         return long_name.title()
+    def read_odometer(self):
+        """Print a statement showing the car's mileage."""
+        print(f"This car has {self.odometer_reading} miles on it.")
+    def update_odometer(self,mileage):
+        """set the odometer reading to the given value.
+        Reject the change if it attempts to roll the odometer back."""
+        if mileage>=self.odometer_reading:
+            self.odometer_reading=mileage
+        else:
+            print("You can't roll back an odometer")
+    def increment_odometer(self,miles):
+        """Add the given amount to the odometer reading."""
+        if miles < 0:
+            print("We can't roll back an odometer")
+        else:
+            self.odometer_reading+=miles
 my_new_car=Car('audi','a4',2019)
 print(my_new_car.get_descriptive_name())
+my_new_car.update_odometer(110)
+my_new_car.read_odometer()
+print("\n")
+my_used_car=Car('subaru','outback',2015)
+print(my_used_car.get_descriptive_name())
+my_used_car.update_odometer(23_500)
+my_used_car.read_odometer()
+my_used_car.increment_odometer(20)
+my_used_car.read_odometer()
     
