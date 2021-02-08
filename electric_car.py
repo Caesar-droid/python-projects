3d242a51d3e89c1f7e29abe7fa6c923a68f01783
domie <dominictoroitichh@gmail.com>
2021-02-08 21:55:17 +0300

    importing a module into a module

--- electric_car.py
+++ electric_car.py
@@ -1,24 +1,5 @@
 #Implementing inheritance 
-class Car:
-   """A simple attempt to represent a car."""
-   def __init__(self,make,model,year):
-       self.make=make
-       self.model=model
-       self.year=year
-       self.odometer_reading=0
-   def get_descriptive_name(self):
-        """Return a neatly formatted descriptive name."""
-        long_name=f"{self.year} {self.make} {self.model}"
-        return long_name.title()
-   def reading_odometer(self):
-        print(f"This car has {self.odometer_reading} miles on it.")
-   def update_odometer(self,mileage):
-        if mileage>=self.odometer_reading:
-            self.odometer_reading=mileage
-        else:
-            print("You can't roll back the odometer!")
-   def increment_odometer(self,miles):
-        self.odometer_reading+=miles
+from car import Car
 class Battery:
     """A simple attempt to model a battery for an electric car."""
     def __init__(self,battery_size=75):
