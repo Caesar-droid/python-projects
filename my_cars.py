0f815f8de0fbe20a858b920e40102196cb2e3eed
domie <dominictoroitichh@gmail.com>
2021-02-08 13:20:51 +0300

    importing an entire module

---
+++ my_cars.py
@@ -0,0 +1,5 @@
+import electric_car
+my_beetle=electric_car.Car('volkswagen','beetle',2019)
+print(my_beetle.get_descriptive_name())
+my_tesla=electric_car.ElectricCar('tesla','roadster',2019)
+print(my_tesla.get_descriptive_name())
