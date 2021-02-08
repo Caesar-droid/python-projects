e60f3e318fceeff71eb8bdecc3d8b40423b9ee34
domie <dominictoroitichh@gmail.com>
2021-02-08 13:21:33 +0300

    implementing classes ...modules

--- my_car.py
+++ my_car.py
@@ -1,5 +1,10 @@
-from car import Car
+from electric_car import Car,ElectricCar
 my_new_car=Car('audi','a4',2019)
 print(my_new_car.get_descriptive_name())
-my_new_car.odometer_reading=23
-my_new_car.read_odometer()
+# my_new_car.odometer_reading=23
+# my_new_car.read_odometer()
+my_bettle=Car('volkswagen','bettle',2019)
+print(my_bettle.get_descriptive_name())
+my_tesla=ElectricCar('tesla','roaster',2019)
+print(my_tesla.get_descriptive_name())
+
