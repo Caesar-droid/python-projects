a56ea7e0589392ce9fdc80f1d2a4b036f08e069d
domie <dominictoroitichh@gmail.com>
2021-02-11 15:33:08 +0300

    reading txt file

--- file_reader.py
+++ file_reader.py
@@ -1,3 +1,15 @@
+#reading an entire file
 with open('text_files/pi_digits.txt')as file_object:
     contents=file_object.read()
 print(contents.rstrip())
+#reading line by line
+filename='text_files/pi_digits.txt'
+with open(filename) as file_object:
+    for line in file_object:
+        print(line.rstrip())
+#making a list of lines from a file
+filename='text_files/pi_digits.txt'
+with open(filename) as file_object:
+    lines=file_object.readlines()
+for line in lines:
+    print(line.rstrip())
