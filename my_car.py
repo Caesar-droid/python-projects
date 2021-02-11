ce6c932dc0d55268ef2a5ae5cbcfe46377bb9fdb
domie <dominictoroitichh@gmail.com>
2021-02-11 12:08:23 +0300

    using an alias on a function

--- my_car.py
+++ my_car.py
@@ -1,10 +1,11 @@
-from electric_car import Car,ElectricCar
+from car import Car
+from electric_car import ElectricCar as EC
 my_new_car=Car('audi','a4',2019)
 print(my_new_car.get_descriptive_name())
 # my_new_car.odometer_reading=23
 # my_new_car.read_odometer()
 my_bettle=Car('volkswagen','bettle',2019)
 print(my_bettle.get_descriptive_name())
-my_tesla=ElectricCar('tesla','roaster',2019)
+my_tesla=EC('tesla','roaster',2019)
 print(my_tesla.get_descriptive_name())
 
