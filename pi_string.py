cd3761f447eea0e252c82d3121012ff428ff9b40
domie <dominictoroitichh@gmail.com>
2021-02-12 13:29:05 +0300

    working with a file's content

---
+++ pi_string.py
@@ -0,0 +1,9 @@
+#working with a file's contents
+filename="text_files/pi_digits.txt"
+with open(filename) as file_object:
+    lines=file_object.readlines()
+pi_string=''
+for line in lines:
+    pi_string+=line.strip()
+print(pi_string)
+print(len(pi_string))
