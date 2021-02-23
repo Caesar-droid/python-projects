6971144d433404b4611314fbe842235eabff1585
domie <dominictoroitichh@gmail.com>
2021-02-23 10:55:32 +0300

    storing in list and appending the text file

--- programming_poll.py
+++ programming_poll.py
@@ -7,4 +7,15 @@ while True:
     else:
         with open(filename,'a') as fo:
             fo.write(response+"\n")
-            
+#solution
+filename='text_files/programming_poll.txt'
+responses=[]
+while True:
+    response=input("\nWhy do you like programming?")
+    responses.append(response)
+    continue_polling=input("would you like to let someone else respond? (y/n)")
+    if continue_polling != 'y':
+        break
+with open (filename,'a') as f:
+    for response in responses:
+        f.write(response+"\n")
