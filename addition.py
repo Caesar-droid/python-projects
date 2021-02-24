b23a5023ce93f18534784b554f6af85053738866
domie <dominictoroitichh@gmail.com>
2021-02-24 13:19:15 +0300

    exception handling

--- addition.py
+++ addition.py
@@ -1,10 +1,11 @@
-try:
-    first=input("Enter First digit: ")
-    first=int(first)
-    second=input("Enter Second digit: ")
-    second=int(second)
-except ValueError:
-    pass
-else:
-    answer = first + second
-    print(f"the result is: {answer}")
+while True:
+    try:
+        first=input("Enter First digit: ")
+        first=int(first)
+        second=input("Enter Second digit: ")
+        second=int(second)
+    except ValueError:
+        pass
+    else:
+        answer = first + second
+        print(f"the result is: {answer}")
