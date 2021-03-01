09355ea84ae72001eb582992591582edad1b8bff
domie <dominictoroitichh@gmail.com>
2021-03-01 09:31:04 +0300

    changed the brackets

--- buffet.py
+++ buffet.py
@@ -1,8 +1,8 @@
-foods=('chapati','githeri','peas','french fries','rice')
+foods=['chapati','githeri','peas','french fries','rice']
 for food in foods:
     print(food)
 #modifying the list of food
-foods=('chapati','githeri','omlette','french fries','peppinos')
+foods=['chapati','githeri','omlette','french fries','peppinos']
 print("\n modified list:")
 for food in foods:
     print(food)
