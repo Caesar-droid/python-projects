5f1fb790ab4d89e9caa195a05d7d24b41accb886
domie <dominictoroitichh@gmail.com>
2021-03-02 12:46:57 +0300

    dealing with json files ...dumping and loading

---
+++ remember_me.py
@@ -0,0 +1,15 @@
+#saving and reading user-generated data
+#load the username, if it has been stored previously
+#otherwise,prompt for the username and store it.
+import json
+filename='username.json'
+try: 
+    with open(filename) as f:
+        username=json.load(f)
+except FileNotFoundError:
+    username=input("What is your name?")
+    with open(filename,'w') as f:
+        json.dump(username,f)
+        print(f"We'll remember you when you come back, {username}!")
+else:
+    print(f"Welcome back,{username}")
