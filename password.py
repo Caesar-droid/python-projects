cfff9f8fe0fe8fc6819f06809815072a9a46c149
domie <dominictoroitichh@gmail.com>
2021-03-31 11:28:29 +0300

    adding more symbols

--- password.py
+++ password.py
@@ -3,7 +3,7 @@ import random
 lower = "abcdefghijklmnopqrstuvwxyz"
 upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 numbers = "0123456789"
-symbols = "[]{}()*;/,._-"
+symbols = "[]{}()*;/,._-£$%^!@~"
 all = lower + upper + numbers + symbols
 length=16
 password="".join(random.sample(all,length))
